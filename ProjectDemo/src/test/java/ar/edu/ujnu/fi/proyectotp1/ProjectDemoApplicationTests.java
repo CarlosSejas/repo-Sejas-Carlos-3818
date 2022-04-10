4a8f918dfd78776bef36959d0eb6628b381e48a1
CarlosSejas <ocarl_14@hotmail.com>
2022-04-10 20:50:52 -0300

    primer commit

---
+++ ProjectDemo/src/test/java/ar/edu/ujnu/fi/proyectotp1/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ujnu.fi.proyectotp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
